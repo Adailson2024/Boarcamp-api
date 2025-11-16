e41f0ed4b5142826e64889423698bd0401aa6d23
ADAILSON OLIVEIRA <adailsonramonmat@gmail.com>
2025-11-16 16:53:26 -0300

    Delete Boardcamp directory

--- Boardcamp/src/errors/errors.js
+++
@@ -1,9 +0,0 @@
-export function minNumberErrors(){
-    return {type:"invalidNumber", 
-    message:"stockTotal e pricePerDay devem ser números maiores que 0"}
-}
-
-export function conflictErrors(entity){
-    return {type:"conflict", 
-    message:`Um ${entity} com esse nome já existe`}
-}
